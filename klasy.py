c7367806680dc773062b1d2dc1071a564aab06c4
bszczerba13 <bartekk29alk@gmail.com>
2026-01-03 10:27:18 +0000

    Added imie to Czlowiek constructor

--- klasy.py
+++ klasy.py
@@ -2,12 +2,17 @@
 class Czlowiek:
     #Istota
     gatunek = "Homo Sapiens"
-    def __init__(self):
+    def __init__(self, imie):
         #Konstruktor
         #Akt istnienia
-        print("Niech powstanie Czlowiek")
+        print(f"Niech powstanie Czlowiek o imieniu {imie}")
+        self.imie = imie
 
 #Powstanie obiektu
 #Gotowanie z przepisu
-adam = Czlowiek()
+adam = Czlowiek("Adam")
+ewa = Czlowiek("Ewa")
 print(adam.gatunek)
+print(ewa.gatunek)
+print(adam.imie)
+print(ewa.imie)
